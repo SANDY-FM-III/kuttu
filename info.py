0844be8b921dfc640356c456763a73bee731d5fe
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-12 21:21:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_CHANNEL = environ.get('AUTH_CHANNEL')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mogodb001:mogodb001@cluster0.ewge2ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://thhsql:thhsql@cluster0.ayxp3sl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Minato")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
