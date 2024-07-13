8509516f105d6bf46b4b246762e86375330c6cad
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-13 11:33:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Minato")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002106576609'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002207785825'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ThappyHour')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), True)
