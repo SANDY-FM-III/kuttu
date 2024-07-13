ebc87677e86d82310df21379f86cbebedbfe2ebb
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-13 09:36:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                        InlineKeyboardButton('🌿 Search Movie File 🌿', switch_inline_query_current_chat='')
+                        InlineKeyboardButton('🔎 Go To PM & Search 🔎', switch_inline_query_current_chat='')
                     ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
