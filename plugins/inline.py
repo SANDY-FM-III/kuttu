4834a9bfcf1f483e17f8b5caa192f6f9bc9ce12b
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-17 10:29:19 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -57,7 +57,12 @@ async def answer(bot, query):
         title=file.file_name
         size=get_size(file.file_size)
         f_caption=file.caption
-        f_caption=CUSTOM_FILE_CAPTION
+        if CUSTOM_FILE_CAPTION:
+        try:
+            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+        except Exception as e:
+            logger.exception(e)
+            f_caption=f_caption
         if f_caption is None:
             f_caption = f"{file.file_name}"
         results.append(
