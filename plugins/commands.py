ee7d229d1f774e308fbae2fbf1f2c84957971b7b
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-17 10:46:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -214,16 +214,17 @@ async def start(client, message):
             file = getattr(msg, filetype.value)
             title = file.file_name
             size=get_size(file.file_size)
-            f_caption = files.caption
+            f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
-                except Exception as e:
-                    logger.exception(e)
-                    f_caption=f_caption
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
+                except:
+                    return
+            await msg.edit_caption(f_caption)
+            return
         except:
             pass
-        return await message.reply('Eʀʀᴏʀ : Lᴏᴏᴋs Lɪᴋᴇ Tʜɪs Fɪʟᴇ Wᴀs Dᴇʟᴇᴛᴇᴅ')
+        return await message.reply('No such file exist.')
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
